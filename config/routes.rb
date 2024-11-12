d18425b645cc56ef7238e6ae62d44e8531eda81e
Daniel Park <intheimageofyeshua@gmail.com>
2024-11-12 23:10:53 +0900

    Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "login/index"
   get "gallery/index"
   get "modal/index"
   get "pricing_cards/index"
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Mulish', ...defaultTheme.fontFamily.sans],
+        mono: ['Rokkitt', 'monospace']
       },
     },
   },
